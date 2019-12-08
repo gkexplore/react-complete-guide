4396f1f02730e8aa97011deffdc52577602a5d00
Karthik Ganesan <gkexplore@gmail.com>
2019-12-07 21:47:04 -0700

    handle dynamic array

--- src/App.js
+++ src/App.js
@@ -47,21 +47,10 @@ class App extends Component {
     if(this.state.showPersons){
       persons = (
         <div>
-            <Person 
-                  name={this.state.persons[0].name} 
-                  age={this.state.persons[0].age}>My Hobbies : Coding</Person>
-            <Person 
-                  name={this.state.persons[1].name} 
-                  age={this.state.persons[1].age}/>
-            <Person 
-                  name={this.state.persons[2].name} 
-                  age={this.state.persons[2].age}
-                  click={this.switchNameHandler.bind(this, 'Karthik Ganesan')}
-                  changed={this.nameChangedHandler}/>
-            <Person 
-                  name={this.state.persons[3].name} 
-                  age={this.state.persons[3].age}/>
-          </div>
+          {this.state.persons.map(person => {
+            return <Person name={person.name} age={person.age}/>
+        })}
+        </div>
       );
     }
 
